cfac9ea4e0eb25406b0254882ad5574a097675a9
yashsavani <yash.savani@gmail.com>
2015-08-02 19:10:46 -0700

    Removed unnecessary files.

--- .build_release/src/caffe/internal_thread.d
+++
@@ -1,9 +0,0 @@
-.build_release/src/caffe/internal_thread.o: src/caffe/internal_thread.cpp \
-  include/caffe/internal_thread.hpp include/caffe/common.hpp \
-  include/caffe/util/device_alternate.hpp
-
-include/caffe/internal_thread.hpp:
-
-include/caffe/common.hpp:
-
-include/caffe/util/device_alternate.hpp:
--- .build_release/src/caffe/layers/hungarian_loss_layer.d
+++
@@ -1,2 +0,0 @@
-.build_release/src/caffe/layers/hungarian_loss_layer.o: \
-  src/caffe/layers/hungarian_loss_layer.cpp

60e3a6af1ad1437033dd58472a5d02cae0dd1283
yashsavani <yash.savani@gmail.com>
2015-08-02 19:10:35 -0700

    Removed unnecessary files.

---
+++ .build_release/src/caffe/layers/hungarian_loss_layer.d
@@ -0,0 +1,2 @@
+.build_release/src/caffe/layers/hungarian_loss_layer.o: \
+  src/caffe/layers/hungarian_loss_layer.cpp
---
+++ .build_release/src/caffe/layers/memory_data_layer.d
@@ -0,0 +1,167 @@
+.build_release/src/caffe/layers/memory_data_layer.o: \
+  src/caffe/layers/memory_data_layer.cpp include/caffe/data_layers.hpp \
+  /Users/ysavani/anaconda/include/hdf5.h \
+  /Users/ysavani/anaconda/include/H5public.h \
+  /Users/ysavani/anaconda/include/H5pubconf.h \
+  /Users/ysavani/anaconda/include/H5version.h \
+  /Users/ysavani/anaconda/include/H5api_adpt.h \
+  /Users/ysavani/anaconda/include/H5Apublic.h \
+  /Users/ysavani/anaconda/include/H5Ipublic.h \
+  /Users/ysavani/anaconda/include/H5Opublic.h \
+  /Users/ysavani/anaconda/include/H5Lpublic.h \
+  /Users/ysavani/anaconda/include/H5Tpublic.h \
+  /Users/ysavani/anaconda/include/H5ACpublic.h \
+  /Users/ysavani/anaconda/include/H5Cpublic.h \
+  /Users/ysavani/anaconda/include/H5Dpublic.h \
+  /Users/ysavani/anaconda/include/H5Epublic.h \
+  /Users/ysavani/anaconda/include/H5Epubgen.h \
+  /Users/ysavani/anaconda/include/H5Fpublic.h \
+  /Users/ysavani/anaconda/include/H5FDpublic.h \
+  /Users/ysavani/anaconda/include/H5Gpublic.h \
+  /Users/ysavani/anaconda/include/H5MMpublic.h \
+  /Users/ysavani/anaconda/include/H5Ppublic.h \
+  /Users/ysavani/anaconda/include/H5Zpublic.h \
+  /Users/ysavani/anaconda/include/H5PLpublic.h \
+  /Users/ysavani/anaconda/include/H5Rpublic.h \
+  /Users/ysavani/anaconda/include/H5Spublic.h \
+  /Users/ysavani/anaconda/include/H5FDcore.h \
+  /Users/ysavani/anaconda/include/H5FDfamily.h \
+  /Users/ysavani/anaconda/include/H5FDlog.h \
+  /Users/ysavani/anaconda/include/H5FDmpi.h \
+  /Users/ysavani/anaconda/include/H5FDmpio.h \
+  /Users/ysavani/anaconda/include/H5FDmulti.h \
+  /Users/ysavani/anaconda/include/H5FDsec2.h \
+  /Users/ysavani/anaconda/include/H5FDstdio.h \
+  /Users/ysavani/anaconda/include/H5FDdirect.h include/caffe/blob.hpp \
+  include/caffe/common.hpp include/caffe/util/device_alternate.hpp \
+  include/caffe/tensor.hpp .build_release/src/caffe/proto/caffe.pb.h \
+  include/caffe/syncedmem.hpp include/caffe/util/math_functions.hpp \
+  include/caffe/util/mkl_alternate.hpp \
+  /usr/local/opt/openblas/include/cblas.h \
+  /usr/local/opt/openblas/include/openblas_config.h \
+  include/caffe/data_transformer.hpp include/caffe/filler.hpp \
+  include/caffe/internal_thread.hpp include/caffe/layer.hpp \
+  include/caffe/layer_factory.hpp include/caffe/util/db.hpp \
+  include/caffe/util/io.hpp /Users/ysavani/anaconda/include/hdf5_hl.h \
+  /Users/ysavani/anaconda/include/H5DOpublic.h \
+  /Users/ysavani/anaconda/include/H5DSpublic.h \
+  /Users/ysavani/anaconda/include/H5LTpublic.h \
+  /Users/ysavani/anaconda/include/H5IMpublic.h \
+  /Users/ysavani/anaconda/include/H5TBpublic.h \
+  /Users/ysavani/anaconda/include/H5PTpublic.h
+
+include/caffe/data_layers.hpp:
+
+/Users/ysavani/anaconda/include/hdf5.h:
+
+/Users/ysavani/anaconda/include/H5public.h:
+
+/Users/ysavani/anaconda/include/H5pubconf.h:
+
+/Users/ysavani/anaconda/include/H5version.h:
+
+/Users/ysavani/anaconda/include/H5api_adpt.h:
+
+/Users/ysavani/anaconda/include/H5Apublic.h:
+
+/Users/ysavani/anaconda/include/H5Ipublic.h:
+
+/Users/ysavani/anaconda/include/H5Opublic.h:
+
+/Users/ysavani/anaconda/include/H5Lpublic.h:
+
+/Users/ysavani/anaconda/include/H5Tpublic.h:
+
+/Users/ysavani/anaconda/include/H5ACpublic.h:
+
+/Users/ysavani/anaconda/include/H5Cpublic.h:
+
+/Users/ysavani/anaconda/include/H5Dpublic.h:
+
+/Users/ysavani/anaconda/include/H5Epublic.h:
+
+/Users/ysavani/anaconda/include/H5Epubgen.h:
+
+/Users/ysavani/anaconda/include/H5Fpublic.h:
+
+/Users/ysavani/anaconda/include/H5FDpublic.h:
+
+/Users/ysavani/anaconda/include/H5Gpublic.h:
+
+/Users/ysavani/anaconda/include/H5MMpublic.h:
+
+/Users/ysavani/anaconda/include/H5Ppublic.h:
+
+/Users/ysavani/anaconda/include/H5Zpublic.h:
+
+/Users/ysavani/anaconda/include/H5PLpublic.h:
+
+/Users/ysavani/anaconda/include/H5Rpublic.h:
+
+/Users/ysavani/anaconda/include/H5Spublic.h:
+
+/Users/ysavani/anaconda/include/H5FDcore.h:
+
+/Users/ysavani/anaconda/include/H5FDfamily.h:
+
+/Users/ysavani/anaconda/include/H5FDlog.h:
+
+/Users/ysavani/anaconda/include/H5FDmpi.h:
+
+/Users/ysavani/anaconda/include/H5FDmpio.h:
+
+/Users/ysavani/anaconda/include/H5FDmulti.h:
+
+/Users/ysavani/anaconda/include/H5FDsec2.h:
+
+/Users/ysavani/anaconda/include/H5FDstdio.h:
+
+/Users/ysavani/anaconda/include/H5FDdirect.h:
+
+include/caffe/blob.hpp:
+
+include/caffe/common.hpp:
+
+include/caffe/util/device_alternate.hpp:
+
+include/caffe/tensor.hpp:
+
+.build_release/src/caffe/proto/caffe.pb.h:
+
+include/caffe/syncedmem.hpp:
+
+include/caffe/util/math_functions.hpp:
+
+include/caffe/util/mkl_alternate.hpp:
+
+/usr/local/opt/openblas/include/cblas.h:
+
+/usr/local/opt/openblas/include/openblas_config.h:
+
+include/caffe/data_transformer.hpp:
+
+include/caffe/filler.hpp:
+
+include/caffe/internal_thread.hpp:
+
+include/caffe/layer.hpp:
+
+include/caffe/layer_factory.hpp:
+
+include/caffe/util/db.hpp:
+
+include/caffe/util/io.hpp:
+
+/Users/ysavani/anaconda/include/hdf5_hl.h:
+
+/Users/ysavani/anaconda/include/H5DOpublic.h:
+
+/Users/ysavani/anaconda/include/H5DSpublic.h:
+
+/Users/ysavani/anaconda/include/H5LTpublic.h:
+
+/Users/ysavani/anaconda/include/H5IMpublic.h:
+
+/Users/ysavani/anaconda/include/H5TBpublic.h:
+
+/Users/ysavani/anaconda/include/H5PTpublic.h:
